a2f63996b28045eb0b8281177835f3cb0f07b761
jaisriv788 <srivastavajai845@gmail.com>
2025-11-04 11:48:52 +0530

    feature: added the model for the assets present with us

--- src/components/dashboard/WalletModelAssetsView.tsx
+++ src/components/dashboard/WalletModelAssetsView.tsx
@@ -1,7 +1,68 @@
-import React from "react";
+import type { RootState } from "@/store/store";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
+interface Assets {
+  total_usdt: string;
+  total_usdc: string;
+}
 const WalletModelAssetsView: React.FC = () => {
-  return <div>WalletModelAssetsView</div>;
+  const [assetsData, setAssetsData] = useState<Assets | null>(null);
+  const baseUrl = useSelector((state: RootState) => state.consts.baseUrl);
+  const userData = useSelector((state: RootState) => state.user.userData);
+  const token = useSelector((state: RootState) => state?.user?.token);
+
+  const fetchAssets = async () => {
+    const response = await axios.post(
+      `${baseUrl}/user-currency-list`,
+      {
+        user_id: userData?.id,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    console.log(response.data.data);
+
+    setAssetsData(response.data.data);
+  };
+
+  useEffect(() => {
+    fetchAssets();
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex items-center gap-3 cursor-pointer transition ease-in-out duration-300 rounded-lg hover:bg-gray-300 px-3 py-1">
+        <img src="usdt.svg" className="w-10 aspect-square" />
+        <div className="flex flex-col">
+          <span className="font-bold">USDT</span>
+          <span className="font-semibold">
+            {!assetsData
+              ? "Loading..."
+              : `$${parseFloat(assetsData?.total_usdt).toFixed(2) ?? 0.0}`}
+          </span>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-3 cursor-pointer transition ease-in-out duration-300 rounded-lg hover:bg-gray-300 px-3 py-1">
+        <img src="usdc.svg" className="w-10 aspect-square" />
+        <div className="flex flex-col">
+          <span className="font-bold">USDC</span>
+          <span className="font-semibold">
+            {!assetsData
+              ? "Loading..."
+              : `$${parseFloat(assetsData?.total_usdc).toFixed(2) ?? 0.0}`}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WalletModelAssetsView;
--- src/components/login/LoginDialog.tsx
+++ src/components/login/LoginDialog.tsx
@@ -41,6 +41,7 @@ export function LoginDialog() {
   const [view, setView] = useState(0);
   const [otp, setOtp] = useState("");
   const [otpLoader, setOtpLoader] = useState(false);
+  const [loadingButton, setLoadingButton] = useState<string | null>(null);
   const [emailVerificationLoader, setEmailVerificationLoader] = useState(false);
   const [numberVerificationLoader, setNumberVerificationLoader] =
     useState(false);
@@ -53,6 +54,7 @@ export function LoginDialog() {
   //Oauth google
   const handleGoogleLogin = async () => {
     try {
+      setLoadingButton("google");
       const response = await axios.get(`${baseUrl}/auth/google`);
 
       if (response.status !== 200) {
@@ -64,11 +66,17 @@ export function LoginDialog() {
       showError("Google Authentication Failed.", "");
       console.log(error);
     }
+    // finally {
+    //   setTimeout(() => {
+    //     setLoadingButton(null);
+    //   }, 1000);
+    // }
   };
 
   //Oauth github
   const handleGithubLogin = async () => {
     try {
+      setLoadingButton("github");
       const response = await axios.get(`${baseUrl}/auth/github`);
 
       if (response.status !== 200) {
@@ -80,11 +88,15 @@ export function LoginDialog() {
       showError("Github Authentication Failed.", "");
       console.log(error);
     }
+    // finally {
+    //   setLoadingButton(null);
+    // }
   };
 
   //Oauth linkedin
   const handleLinkedinLogin = async () => {
     try {
+      setLoadingButton("linkedin");
       const response = await axios.get(`${baseUrl}/auth/github`);
 
       if (response.status !== 200) {
@@ -96,11 +108,15 @@ export function LoginDialog() {
       showError("LinkedIn Authentication Failed.", "");
       console.log(error);
     }
+    // finally {
+    //   setLoadingButton(null);
+    // }
   };
 
   //Oauth facebook
   const handleFacebookLogin = async () => {
     try {
+      setLoadingButton("facebook");
       const response = await axios.get(`${baseUrl}/auth/github`);
 
       if (response.status !== 200) {
@@ -112,6 +128,9 @@ export function LoginDialog() {
       showError("Facebook Authentication Failed.", "");
       console.log(error);
     }
+    // finally {
+    //   setLoadingButton(null);
+    // }
   };
 
   //Sending otp to email
@@ -280,6 +299,7 @@ export function LoginDialog() {
     dispatch(setIsUserConnected({ isConnected: true }));
     navigate("/dashboard");
   }
+
   return (
     <Dialog
       open={open}
@@ -359,83 +379,122 @@ export function LoginDialog() {
 
             <Button
               onClick={handleGoogleLogin}
-              className="relative flex items-center justify-center w-full cursor-pointer py-6 border border-gray-300 bg-white rounded-md shadow-sm hover:shadow-md hover:bg-gray-50 transition-all duration-300 ease-in-out"
+              disabled={loadingButton !== null && loadingButton !== "google"}
+              className="disabled:bg-gray-200 relative flex items-center justify-center w-full cursor-pointer py-6 border border-gray-300 bg-white rounded-md shadow-sm hover:shadow-md hover:bg-gray-50 transition-all duration-300 ease-in-out"
             >
-              {/* Google "G" logo (SVG) */}
-              <svg
-                className="w-5 h-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 48 48"
-              >
-                <path
-                  fill="#EA4335"
-                  d="M24 9.5c3.94 0 7.47 1.5 10.23 3.97l7.61-7.61C37.24 1.9 30.97 0 24 0 14.62 0 6.53 5.05 2.48 12.46l8.9 6.92C13.1 13.67 18.14 9.5 24 9.5z"
-                />
-                <path
-                  fill="#34A853"
-                  d="M46.98 24.5c0-1.57-.14-3.08-.41-4.54H24v9.04h12.97c-.56 2.96-2.2 5.46-4.66 7.14l7.2 5.59c4.19-3.87 6.47-9.57 6.47-16.23z"
-                />
-                <path
-                  fill="#4A90E2"
-                  d="M9.37 28.62a14.42 14.42 0 0 1-.76-4.62c0-1.6.27-3.14.76-4.62l-8.9-6.92A23.933 23.933 0 0 0 0 24c0 3.89.93 7.56 2.48 10.92l8.9-6.92z"
-                />
-                <path
-                  fill="#FBBC05"
-                  d="M24 48c6.48 0 11.9-2.13 15.86-5.77l-7.2-5.59c-2.01 1.35-4.6 2.15-8.66 2.15-5.86 0-10.9-4.17-12.62-9.88l-8.9 6.92C6.53 42.95 14.62 48 24 48z"
-                />
-              </svg>
+              {loadingButton == "google" ? (
+                <>
+                  <Spinner className="text-gray-700 size-6" />
+                  <span className="text-gray-700">Signing In</span>
+                </>
+              ) : (
+                <>
+                  <svg
+                    className="w-5 h-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 48 48"
+                  >
+                    <path
+                      fill="#EA4335"
+                      d="M24 9.5c3.94 0 7.47 1.5 10.23 3.97l7.61-7.61C37.24 1.9 30.97 0 24 0 14.62 0 6.53 5.05 2.48 12.46l8.9 6.92C13.1 13.67 18.14 9.5 24 9.5z"
+                    />
+                    <path
+                      fill="#34A853"
+                      d="M46.98 24.5c0-1.57-.14-3.08-.41-4.54H24v9.04h12.97c-.56 2.96-2.2 5.46-4.66 7.14l7.2 5.59c4.19-3.87 6.47-9.57 6.47-16.23z"
+                    />
+                    <path
+                      fill="#4A90E2"
+                      d="M9.37 28.62a14.42 14.42 0 0 1-.76-4.62c0-1.6.27-3.14.76-4.62l-8.9-6.92A23.933 23.933 0 0 0 0 24c0 3.89.93 7.56 2.48 10.92l8.9-6.92z"
+                    />
+                    <path
+                      fill="#FBBC05"
+                      d="M24 48c6.48 0 11.9-2.13 15.86-5.77l-7.2-5.59c-2.01 1.35-4.6 2.15-8.66 2.15-5.86 0-10.9-4.17-12.62-9.88l-8.9 6.92C6.53 42.95 14.62 48 24 48z"
+                    />
+                  </svg>
 
-              <span className="font-medium text-gray-700 text-base">
-                Sign in with Google
-              </span>
+                  <span className="font-medium text-gray-700 text-base">
+                    Sign in with Google
+                  </span>
+                </>
+              )}
             </Button>
 
             <Button
               onClick={handleGithubLogin}
-              className="relative flex items-center justify-center cursor-pointer w-full py-6 border border-gray-300 bg-white rounded-md shadow-sm hover:shadow-md hover:bg-gray-50 transition-all duration-300 ease-in-out"
+              disabled={loadingButton !== null && loadingButton !== "github"}
+              className="disabled:bg-gray-200 relative flex items-center justify-center cursor-pointer w-full py-6 border border-gray-300 bg-white rounded-md shadow-sm hover:shadow-md hover:bg-gray-50 transition-all duration-300 ease-in-out"
             >
-              <svg
-                className="w-5 h-5 mr-3 fill-current text-gray-800"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 0C5.37 0 0 5.37 0 12a12 12 0 008.21 11.43c.6.11.82-.26.82-.58v-2.04c-3.34.73-4.04-1.61-4.04-1.61-.54-1.38-1.32-1.74-1.32-1.74-1.08-.74.08-.73.08-.73 1.19.08 1.82 1.23 1.82 1.23 1.06 1.82 2.78 1.29 3.46.99.11-.77.42-1.29.76-1.59-2.66-.3-5.47-1.33-5.47-5.91 0-1.31.47-2.38 1.23-3.22-.12-.3-.54-1.52.12-3.17 0 0 1-.32 3.3 1.23A11.5 11.5 0 0112 6.8c1.02 0 2.05.14 3.01.4 2.3-1.55 3.3-1.23 3.3-1.23.66 1.65.24 2.87.12 3.17.77.84 1.23 1.91 1.23 3.22 0 4.59-2.81 5.6-5.49 5.9.43.37.81 1.1.81 2.22v3.29c0 .32.21.7.83.58A12 12 0 0024 12c0-6.63-5.37-12-12-12z" />
-              </svg>
-              <span className="font-medium text-gray-700 text-base">
-                Sign in with GitHub
-              </span>
+              {loadingButton == "github" ? (
+                <>
+                  <Spinner className="text-gray-700 size-6" />
+                  <span className="text-gray-700">Signing In</span>
+                </>
+              ) : (
+                <>
+                  <svg
+                    className="w-5 h-5 mr-3 fill-current text-gray-800"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 0C5.37 0 0 5.37 0 12a12 12 0 008.21 11.43c.6.11.82-.26.82-.58v-2.04c-3.34.73-4.04-1.61-4.04-1.61-.54-1.38-1.32-1.74-1.32-1.74-1.08-.74.08-.73.08-.73 1.19.08 1.82 1.23 1.82 1.23 1.06 1.82 2.78 1.29 3.46.99.11-.77.42-1.29.76-1.59-2.66-.3-5.47-1.33-5.47-5.91 0-1.31.47-2.38 1.23-3.22-.12-.3-.54-1.52.12-3.17 0 0 1-.32 3.3 1.23A11.5 11.5 0 0112 6.8c1.02 0 2.05.14 3.01.4 2.3-1.55 3.3-1.23 3.3-1.23.66 1.65.24 2.87.12 3.17.77.84 1.23 1.91 1.23 3.22 0 4.59-2.81 5.6-5.49 5.9.43.37.81 1.1.81 2.22v3.29c0 .32.21.7.83.58A12 12 0 0024 12c0-6.63-5.37-12-12-12z" />
+                  </svg>
+                  <span className="font-medium text-gray-700 text-base">
+                    Sign in with GitHub
+                  </span>
+                </>
+              )}
             </Button>
 
             <Button
               onClick={handleLinkedinLogin}
-              className="relative flex items-center justify-center cursor-pointer w-full py-6 border border-gray-300 bg-white rounded-md shadow-sm hover:shadow-md hover:bg-gray-50 transition-all duration-300 ease-in-out"
+              disabled={loadingButton !== null && loadingButton !== "linkedin"}
+              className="disabled:bg-gray-200 relative flex items-center justify-center cursor-pointer w-full py-6 border border-gray-300 bg-white rounded-md shadow-sm hover:shadow-md hover:bg-gray-50 transition-all duration-300 ease-in-out"
             >
-              <svg
-                className="w-5 h-5 mr-3 fill-[#0A66C2]"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M19 0h-14c-2.76 0-5 2.24-5 5v14a5 5 0 005 5h14a5 5 0 005-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-9h3v9zm-1.5-10.29c-.97 0-1.75-.79-1.75-1.76S5.53 5.2 6.5 5.2 8.25 6 8.25 6.95c0 .97-.78 1.76-1.75 1.76zM20 19h-3v-4.74c0-1.13-.02-2.58-1.57-2.58-1.58 0-1.82 1.23-1.82 2.5V19h-3v-9h2.88v1.23h.04c.4-.75 1.38-1.54 2.85-1.54 3.04 0 3.6 2 3.6 4.6V19z" />
-              </svg>
-              <span className="font-medium text-gray-700 text-base">
-                Sign in with LinkedIn
-              </span>
+              {loadingButton == "linkedin" ? (
+                <>
+                  <Spinner className="text-gray-700 size-6" />
+                  <span className="text-gray-700">Signing In</span>
+                </>
+              ) : (
+                <>
+                  <svg
+                    className="w-5 h-5 mr-3 fill-[#0A66C2]"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M19 0h-14c-2.76 0-5 2.24-5 5v14a5 5 0 005 5h14a5 5 0 005-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-9h3v9zm-1.5-10.29c-.97 0-1.75-.79-1.75-1.76S5.53 5.2 6.5 5.2 8.25 6 8.25 6.95c0 .97-.78 1.76-1.75 1.76zM20 19h-3v-4.74c0-1.13-.02-2.58-1.57-2.58-1.58 0-1.82 1.23-1.82 2.5V19h-3v-9h2.88v1.23h.04c.4-.75 1.38-1.54 2.85-1.54 3.04 0 3.6 2 3.6 4.6V19z" />
+                  </svg>
+                  <span className="font-medium text-gray-700 text-base">
+                    Sign in with LinkedIn
+                  </span>
+                </>
+              )}
             </Button>
 
             <Button
               onClick={handleFacebookLogin}
-              className="relative flex items-center justify-center cursor-pointer w-full py-6 border border-gray-300 bg-white rounded-md shadow-sm hover:shadow-md hover:bg-gray-50 transition-all duration-300 ease-in-out"
+              disabled={loadingButton !== null && loadingButton !== "facebook"}
+              className="disabled:bg-gray-200 relative flex items-center justify-center cursor-pointer w-full py-6 border border-gray-300 bg-white rounded-md shadow-sm hover:shadow-md hover:bg-gray-50 transition-all duration-300 ease-in-out"
             >
-              <svg
-                className="w-5 h-5 mr-3 fill-[#1877F2]"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M22.675 0h-21.35C.597 0 0 .6 0 1.333v21.333C0 23.4.597 24 1.325 24H12v-9.294H9.294v-3.622H12V8.413c0-2.68 1.634-4.141 4.022-4.141 1.14 0 2.12.084 2.405.122v2.79h-1.651c-1.295 0-1.546.616-1.546 1.52v1.982h3.09l-.403 3.622h-2.687V24h5.27C23.403 24 24 23.4 24 22.667V1.333C24 .6 23.403 0 22.675 0z" />
-              </svg>
-              <span className="font-medium text-gray-700 text-base">
-                Sign in with Facebook
-              </span>
+              {loadingButton == "facebook" ? (
+                <>
+                  <Spinner className="text-gray-700 size-6" />
+                  <span className="text-gray-700">Signing In</span>
+                </>
+              ) : (
+                <>
+                  <svg
+                    className="w-5 h-5 mr-3 fill-[#1877F2]"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M22.675 0h-21.35C.597 0 0 .6 0 1.333v21.333C0 23.4.597 24 1.325 24H12v-9.294H9.294v-3.622H12V8.413c0-2.68 1.634-4.141 4.022-4.141 1.14 0 2.12.084 2.405.122v2.79h-1.651c-1.295 0-1.546.616-1.546 1.52v1.982h3.09l-.403 3.622h-2.687V24h5.27C23.403 24 24 23.4 24 22.667V1.333C24 .6 23.403 0 22.675 0z" />
+                  </svg>
+                  <span className="font-medium text-gray-700 text-base">
+                    Sign in with Facebook
+                  </span>
+                </>
+              )}
             </Button>
 
             <div className="flex md:flex-row flex-col gap-3">
